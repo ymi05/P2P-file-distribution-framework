f3d7e2f5a6683f1cab1bf9f70d283aa110b90af7
Youssef Itani <Youssef.itani@gmail.com>
2020-10-31 23:44:25 +0200

    Modified the peer class and tracker class. We can also send files.

--- peer.py
+++ peer.py
@@ -1,15 +1,51 @@
+from socket import *
+
+
 class Peer():
     peerCounter = 0
 
     def __init__(self, name):
-        self.peerID = peerCounter
+        self.__peerID__ = Peer.peerCounter
         Peer.peerCounter += 1
-        self.name = name
+        self.__peerName__ = name
         self.fileChunksSaved = None
+        self.peerSocket = None
         self.network = None
 
-    def requestFile(self):
-        pass
+    def connect(self, host="127.0.0.1", port=5000):
+        self.peerSocket = socket(AF_INET, SOCK_STREAM)
+        self.peerSocket.connect((host, port))
+
+    def requestFile(self, fileName):
+
+        if fileName != "q":  # to quit
+            self.peerSocket.send(fileName.encode())
+            data = self.peerSocket.recv(1024).decode()
+
+            if data[:6] == "EXISTS":  # server tells us if the file exists and sedns the size
+                filesize = int(data[6:])
+                message = input(
+                    f"File Exists , {filesize} Bytes. Download?(Y/N)")
+
+                if message == "Y":
+                    self.peerSocket.send("OK".encode())
+                    f = open(f"Client_downloads/new_{fileName}", "wb")
+                    data = self.peerSocket.recv(1024)
+
+                    totalReceived = len(data)
+                    f.write(data)
+                    while totalReceived < filesize:  # in case the file is bigger than 1024 bytes, we keep checking if the total recieved is equal to the actual file size
+                        data = self.peerSocket.recv(1024)
+
+                        totalReceived += len(data)
+                        f.write(data)
+                        print("{0:.2f").format(
+                            (totalReceived / float(filesize) * 100) + "%Done")
+                    f.close()
+                    print("Download Complete")
+
+            else:
+                print("File does not exist!")
 
     def sendChunks(self, requestedFileChunks):
         pass
@@ -19,3 +55,11 @@ class Peer():
 
     def joinNetwork(self):
         pass
+
+    @property
+    def id(self):
+        return self.__peerID__
+
+    @property
+    def name(self):
+        return self.__peerName__
--- tracker.py
+++ tracker.py
@@ -1,22 +1,69 @@
+from socket import *
+import threading
+import os
+
+
 class Tracker():
     trackerCounter = 0
 
     def __init__(self):
-        self.trackerID = trackerCounter
+        self.trackerID = Tracker.trackerCounter
         Tracker.trackerCounter += 1
         self.manifestFile = None
+        self.trackerSocket = None
 
-    def fileExists(self, fileName):
-        pass
+    def setSocket(self, host="127.0.0.1", port=5000):
+        self.trackerSocket = socket(AF_INET, SOCK_STREAM)
+        self.trackerSocket.bind((host, port))
+        self.trackerSocket.listen(5)
+        self.runServer()
+
+    def runServer(self):
+        print("Server started.")
+        while True:
+            connection, addr = self.trackerSocket.accept()
+            print(f"Client connected IP < {addr} >")
+            t = threading.Thread(target=self.sendFile,
+                                 args=("sendingThread", connection))
+            t.start()
+        self.trackerSocket.close()
+
+    @staticmethod
+    def fileExists(fileName):
+        return os.path.isfile(fileName)
 
     def sendManifestFile(self, volunteerID):
         pass
 
-    def sendFile(self, fileName, volunteerID):
-        pass
+    def sendFile(self, name,  connection):
+        fileName = connection.recv(1024)
+        if Tracker.fileExists(fileName):
+            connection.send(f"EXISTS {os.path.getsize(fileName)}".encode())
+            userResponse = connection.recv(1024).decode()
+            if userResponse[:2] == "OK":
+                with open(fileName, 'rb') as f:  # we read the file as bytes
+                    bytesToSend = f.read(1024)
+                    connection.send(bytesToSend)
+                    while bytesToSend != "":  # since we cannot garuntee that the file size will be 1024 bytes, we keep sending until there is nothing
+                        bytesToSend = f.read(1024)
+                        connection.send(bytesToSend)
+
+        else:
+            connection.send("ERR".encode())
+
+        connection.close()
 
     def deleteFile(self, fileName):
         pass
 
     def sendChunksToVolunteer(self, data):
         pass
+
+
+def Main():
+    server = Tracker()
+    server.setSocket()
+
+
+if __name__ == "__main__":
+    Main()
